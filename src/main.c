ae97d3b5e7653931bce485b9d280bd4cd9fa5a6b
bps <bps@arcturus.su>
2012-08-16 04:25:30 +0100

    yea

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "util.h"
 #include "set.h"
 #include "list.h"
 #include "formula.h"
@@ -34,7 +35,7 @@ void printformula(int *formula)
 }
 
 /* generate ALL balanced formulae of n variables */
-int **genbf(int n)
+int **genbfold(int n)
 {
     int s[n], m[n]; s[0] = -1;
     int i, j, k, l; /* we do a lot of counting... */
@@ -144,21 +145,161 @@ int **genbf2(int n)
     return exhaust(conj, NULL);
 }
 
+void genbf_i(int n, int m, int *maxima, int *choices, int *choice, int j)
+{
+    static int i;
+
+    if(j == n) {
+        //choices[i] = malloc(n*sizeof(int));
+        memcpy(&choices[i*n], choice, n*sizeof(int));
+        i++;
+        return;
+    }
+    else if(j < 0) { /* initial call */
+        /* initialize choice 2d array */
+        /*int m = n;
+        for(i = 0; i < n; i++) m *= maxima[i];*/
+
+        /* and counting variables... */
+        i = 0;
+        j = 0;
+    }
+    else if(i == m) return; // TODO: I think this is unnecessary
+
+
+    int k;
+    for(k = 0; k < maxima[j]; k++) {
+        choice[j] = k;
+        genbf_i(n, m, maxima, choices, choice, j+1);
+    }
+
+    return;
+}
+
+int **genbf_(int *atoms, int n, enum OPERATOR op, int *n_ret)
+{
+    /* base case, which is probably horribly inefficient... maybe I could cut
+     * down on so much dynamic allocation, but we'll see... */
+    if(n == 1) {
+        int **ret = malloc(2*sizeof(int *));
+        ret[0] = malloc(2);
+        // TODO: again, better way to do this?
+        ret[0][0] = atoms[0]; ret[0][1] = OP_FIN;
+        ret[1] = NULL;
+        *n_ret = 1;
+
+        return ret;
+    }
+
+    int i, j, k;
+    int **ret = NULL;
+    *n_ret = 0;
+    enum OPERATOR op2 = op == OP_OR ? OP_AND : OP_OR;
+
+    int partsize, s[n], m[n]; s[0] = -1;
+    while((partsize = partition(n, s, m))) {
+        /* this causes an infinite loop, and the case of the operator acting
+         * directly on the passed atoms is covered instead by the case when
+         * partsize is n instead */
+        if(partsize == 1) continue;
+
+        int **subbfs[partsize];
+        int maxima[partsize], atoms2[n];
+        int n_choices = 1;
+        for(i = 0; i < partsize; i++) {
+            for(j = 0, k = 0; j < n; j++)
+                if(s[j] == i+1) atoms2[k++] = atoms[j];
+            subbfs[i] = genbf_(atoms2, k, op2, &maxima[i]);
+            //maxima[i] = length_l(subbfs[i]);
+            n_choices *= maxima[i];
+        }
+
+        /* a 2d array would be nice but C sucks at such things when passing
+         * between functions and I don't really want to malloc all that shit
+         * and then have to free it */
+        int choices[n_choices*partsize];
+        int choice[partsize];
+        genbf_i(partsize, n_choices, maxima, choices, choice, -1);
+
+
+        struct range_t t; t.top = 0;
+        for(i = 0; i < n_choices; i++) {
+            int *result = malloc(3*sizeof(int));
+            /* TODO: neater way to do this? */
+            result[0] = op; result[1] = OP_CLOSE; result[2] = OP_FIN;
+
+            /* we put them in reverse to preserve lexicographical order, since
+             * shove() is a quite primitive */
+            for(j = partsize-1; j >= 0; j--)
+                shove(result, &t, subbfs[j][choices[i*partsize+j]]);
+
+            /* now shove it onto our list to return */
+            ret = realloc(ret, (++(*n_ret)+1)*sizeof(int *));
+            ret[(*n_ret)-1] = result;
+            ret[*n_ret] = NULL;
+
+            /* and now we're done with coice i, so free it */
+            //free(choices[i]);
+        }
+
+        /* free up the rest of the stuff... */
+        //for(i = 0; i < partsize; i++) free(subbfs[i]);
+    }
+
+    return ret;
+}
+
+int **genbf(int n)
+{
+    int i, j;
+    int atoms[n];
+    for(i = 0; i < n; i++) atoms[i] = i;
+
+    int n_ands, n_ors;
+    int **ands = genbf_(atoms, n, OP_AND, &n_ands);
+    int **ors = genbf_(atoms, n, OP_OR, &n_ors);
+
+    int **ret = malloc((n_ands+n_ors+1)*sizeof(int *));
+
+    /* this looks stupid but this way we alternate AND and OR thingies, which I
+     * think is probably more sensical... code to just shove in all the ANDs
+     * and then all the ORs is commented out after this stuff though */
+    for(i = 0, j = 0; i < min(n_ands, n_ors); i++) {
+        ret[j++] = ands[i];
+        ret[j++] = ors[i];
+    }
+
+    if(n_ands > n_ors)
+        for( ; i < n_ands; i++) ret[j++] = ands[i];
+    else if(n_ors > n_ands)
+        for( ; i < n_ors; i++) ret[j++] = ors[i];
+
+    /*
+    for(i = 0, k = 0; i < n_ands; i++) ret[j++] = ands[i];
+    for(i = 0; i < n_ors; i++) ret[j++] = ors[i];
+    */
+
+    free(ands);
+    free(ors);
+
+    return ret;
+}
+
 int main(int argc, char *argv[])
 {
-    int **bfs = genbf2(7);
+    int **bfs = genbf(3);
     int i, j;
     int n_bfs = length_l((void **)bfs);
     int totalinf = 0;
     int totaltriv = 0;
 
-    FILE *fh = fopen("./bfs.7", "w+");
+    //FILE *fh = fopen("./bfs.7", "w+");
     for(i = 0; i < n_bfs; i++) {
         printf("writing %d/%d\n", i+1, n_bfs);
-        fwrite(bfs[i], sizeof(int), length(bfs[i])+1, fh);
+        //fwrite(bfs[i], sizeof(int), length(bfs[i])+1, fh);
     }
 
-    fclose(fh);
+    //fclose(fh);
     exit(0);
     //for(i = 0; i < n_bfs; i++) printformula(bfs[i]);
     for(i = 0; i < n_bfs; i++) {
